8859d58e16cdf4c9e5117de0301528fb242d3b46
paninology <paninology@gmail.com>
2022-09-26 10:13:43 +0900

    Initial Commit

---
+++ SeSAC2Step3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2Step3
+//
+//  Created by yongseok lee on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
